4c79c192abd8acebfcf5366ce2843f3475d6ddc6
Luiz Carlos Oliveira Maciel <luiz.carlos@propig.com.br>
2025-02-07 16:16:40 -0300

    [ FEAT ] -> List products in order view

--- src/controller/OrderController.java
+++ src/controller/OrderController.java
@@ -3,6 +3,11 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.IntegerStringConverter;
+import model.Product;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -14,9 +19,43 @@ public class OrderController implements Initializable {
     @FXML
     private ComboBox<String> categoryComboBox;
 
+    @FXML
+    private TableView<Product> productsTable;
+    
+    @FXML
+    private TableColumn<Product, String> categoryColumn;
+
+    @FXML
+    private TableColumn<Product, String> nameColumn;
+
+    @FXML
+    private TableColumn<Product, Integer> quantityColumn;
+
+    @FXML
+    private TableColumn<Product, Double> valueColumn;
+
+    @FXML
+    private TableColumn<Product, Integer> desiredQuantity;
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         loadCategories();
+        loadProducts();
+
+        productsTable.getColumns().forEach(column -> column.setReorderable(false));
+        nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        quantityColumn.setCellValueFactory(cellData -> cellData.getValue().quantityProperty().asObject());
+        valueColumn.setCellValueFactory(cellData -> cellData.getValue().valueProperty().asObject());
+        categoryColumn.setCellValueFactory(cellData -> cellData.getValue().categoryProperty());
+        desiredQuantity.setCellValueFactory(cellData -> cellData.getValue().desiredQuantityProperty().asObject());
+    
+        desiredQuantity.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        desiredQuantity.setEditable(true);
+
+        desiredQuantity.setOnEditCommit(event -> {
+            Product product = event.getRowValue();
+            product.setDesiredQuantity(event.getNewValue());
+        });
     }
 
     private void loadCategories() {
@@ -30,13 +69,46 @@ public class OrderController implements Initializable {
     
             BufferedReader br = new BufferedReader(new FileReader(pathDatabase.toURI().getPath()));
     
-            br.readLine(); // Pular o cabeçalho
+            br.readLine();
     
             String line;
             while ((line = br.readLine()) != null) { 
                 String[] values = line.split(",");
                 if (values.length > 1) {
                     categoryComboBox.getItems().add(values[1]);
+                }
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o arquivo CSV: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void loadProducts() {
+        try {
+            URL pathDatabase = getClass().getResource("../database/Product.csv");
+    
+            if (pathDatabase == null) {
+                System.out.println("Arquivo Product.csv não encontrado! Verifique o caminho.");
+                return;
+            }
+    
+            BufferedReader br = new BufferedReader(new FileReader(pathDatabase.toURI().getPath()));
+
+            br.readLine();
+    
+            String line;
+            while ((line = br.readLine()) != null) { 
+                String[] values = line.split(",");
+                if (values.length > 3) {
+                    Product product = new Product(
+                        values[0], 
+                        values[1],
+                        Integer.parseInt(values[2]),
+                        Double.parseDouble(values[3])
+                    );
+                    productsTable.getItems().add(product);
                 }
             }
             br.close();
---
+++ src/model/Product.java
@@ -0,0 +1,65 @@
+package model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Product {
+
+    private SimpleStringProperty name;
+    private SimpleStringProperty category;
+    private SimpleIntegerProperty quantity;
+    private SimpleDoubleProperty value;
+    private IntegerProperty desiredQuantity;
+
+    public Product(String name, String category, int quantity, double value) {
+        this.name = new SimpleStringProperty(name);
+        this.category = new SimpleStringProperty(category);
+        this.quantity = new SimpleIntegerProperty(quantity);
+        this.value = new SimpleDoubleProperty(value);
+        this.desiredQuantity = new SimpleIntegerProperty(0);
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public StringProperty categoryProperty() {
+        return category;
+    }
+
+    public IntegerProperty quantityProperty() {
+        return quantity;
+    }
+
+    public DoubleProperty valueProperty() {
+        return value;
+    }
+
+    public IntegerProperty desiredQuantityProperty() {
+        return desiredQuantity;
+    }
+
+    public void setName(SimpleStringProperty name) {
+        this.name = name;
+    }
+
+    public void setCategory(SimpleStringProperty category) {
+        this.category = category;
+    }
+
+    public void setQuantity(SimpleIntegerProperty quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setValue(SimpleDoubleProperty value) {
+        this.value = value;
+    }
+
+    public void setDesiredQuantity(int desiredQuantity) {
+        this.desiredQuantity.set(desiredQuantity);
+    }
+}
